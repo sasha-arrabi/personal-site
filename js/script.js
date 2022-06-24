32c14603e955410ace5893985e87902ecef96e2d
sasha-arrabi <dev@sashaarrabi.com>
2022-06-24 15:22:38 -0400

    Update experience section with value-centric language Add naive, client-side implementation of captcha Includes 2 honeypots and a math problem to solve, but no server-side validation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function init() {
     setupScrollPositionObservers();
     setupAboutTypewriter();
     setupContactFormListeners();
+    createDynamicFormCaptcha();
 }
 
 function setupScrollPositionObservers() {
@@ -44,15 +45,50 @@ function setupAboutTypewriter() {
 }
 
 function setupContactFormListeners() {
-    $('contactForm').addEventListener('submit', () => {
-        $('contactFormContainer').style.display = 'none';
-        $('contactFormSubmitted').style.display = 'block';
+    /** @type { HTMLFormElement } */
+    const contactForm = $('contactForm');
+
+    contactForm.addEventListener('submit', (event) => {
+        if (checkForm()) {
+            contactForm.action = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScfII1_3WdYy9eGWvET1DHcPq8cAlf_u81xHcpYsusfDOBqaw/formResponse?embedded=true';
+            $('contactFormContainer').style.display = 'none';
+            $('contactFormSubmitted').style.display = 'block';
+        } else {
+            event.preventDefault();
+        }
     });
     
     const contactResult = $('contactResult');
     contactResult.addEventListener('load', () => {
         contactResult.parentNode.removeChild(contactResult);
     });
+}
+
+function createDynamicFormCaptcha() {
+    $('captchaFirstNumber').innerHTML = Math.round(Math.random() * 10);
+    $('captchaOperation').innerHTML = ['+', '-', 'x'][Math.round(Math.random() * 2)];
+    $('captchaSecondNumber').innerHTML = Math.round(Math.random() * 10);
+}
+
+function checkForm() {
+    return !$('lastNameInput').value && !$('robotCheck').checked && validateFormCaptcha();
+}
+
+function validateFormCaptcha() {
+    const firstNumber = +$('captchaFirstNumber').innerHTML;
+    const operation = $('captchaOperation').innerHTML;
+    const secondNumber = +$('captchaSecondNumber').innerHTML;
+    /** @type { HTMLInputElement } */
+    const captchaInput = $('captchaInput');
+    const multiplyResult = operation !== 'x' || (firstNumber * secondNumber === +captchaInput.value);
+    const addResult = operation !== '+' || (firstNumber + secondNumber === +captchaInput.value);
+    const subtractResult = operation !== '-' || (firstNumber - secondNumber === +captchaInput.value);
+
+    if (multiplyResult && addResult && subtractResult) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 init();
